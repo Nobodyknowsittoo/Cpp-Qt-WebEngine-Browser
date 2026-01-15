981abc683b0dafb453b19f25a7ad92ec6d0b0be9
Nobdyknowsit <antmoh11@gmail.com>
2026-01-15 18:16:57 +0100

    Initializing cmake

---
+++ build-dir/CMakeFiles/QtWebEngineBrowser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtWebEngineBrowser.dir/main.cpp.o"
+  "CMakeFiles/QtWebEngineBrowser.dir/main.cpp.o.d"
+  "QtWebEngineBrowser"
+  "QtWebEngineBrowser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtWebEngineBrowser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-dir/CMakeFiles/QtWebEngineBrowser.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/QtWebEngineBrowser.dir/main.cpp.o: \
+ /home/anonymous/Documents/leBrowser/main.cpp /usr/include/stdc-predef.h
