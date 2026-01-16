cde5b3f987f872084b16ea40e219bfcfd82a947b
Nobdyknowsit <antmoh11@gmail.com>
2026-01-16 20:50:33 +0100

    Test Implementing triigerActions

--- build-dir/CMakeFiles/QtWebEngineBrowser.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/QtWebEngineBrowser.dir/main.cpp.o: \
- /home/anonymous/Documents/leBrowser/main.cpp /usr/include/stdc-predef.h
--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QWebEngineView view;
+    view.page()->triggerAction(QWebEnginePage::Back);
+    view.page()->triggerAction(QWebEnginePage::Forward);
     view.setUrl(QUrl("https://www.qt.io"));
     view.resize(1024, 768);
     view.show();
